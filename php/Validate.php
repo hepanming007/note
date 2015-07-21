4f7a3580eafd24fe036f2af72be259f21ca78909
hepanming007 <hepanming007@126.com>
2015-07-21 17:58:20 +0800

    Update Validate.php

--- php/Validate.php
+++ php/Validate.php
@@ -4,10 +4,44 @@
  */
 class Validate{
 
+     CONST DATE     =   'date';
+     CONST TIME      =   'time';
+     CONST MOBILE    =   'mobile';
+     CONST EMAIL     =   'email';
+     CONST POST_CODE =   'post_code';
+     CONST IP        =   'ip';
+     CONST QQ        =   'qq';
+     CONST ID_CART   =   'id_card';
+     CONST TELEPHONE =   'telephone';
+     CONST URL       =   'url';
+     CONST REQUIRED  =   'required';
+     CONST NUMBERNIC =   'numeric';
+     CONST INTEGER       =   'integer';
+
+
+
+     public  static $errorMessage = array(
+            self::DATE    =>    '日期格式错误',
+            self::TIME    =>    '日期格式错误',
+            self::MOBILE  =>    '手机格式错误',
+            self::EMAIL   =>    '邮箱格式错误',
+            self::POST_CODE =>  '邮政编码错误',
+            self::IP       =>   'IP格式错误',
+            self::QQ       =>   'QQ格式错误',
+            self::ID_CART  =>   '身份账号格式错误',
+            self::TELEPHONE=>   '电话号码错误',
+            self::URL      =>   'URL地址错误',
+            self::REQUIRED =>   '字段必填',
+            self::NUMBERNIC =>  '数值类型',
+            self::INTEGER   =>  '必须为整型',
+     );
+
+
+
     /**
      *  检查日期 xxxx-xx-xx
      */
-    public static function check_date($date = ''){
+    public static function date($date = ''){
         return preg_match('/^[\d]{4}\-[\d]{1,2}-[\d]{1,2}$/', $date);
     }
 
@@ -15,7 +49,7 @@ class Validate{
     /**
      *  检查完整日期 xxxx-xx-xx xx:xx:xx
      */
-    public static function check_time($date = ''){
+    public static function time($date = ''){
         return preg_match('/^[\d]{4}\-[\d]{1,2}-[\d]{1,2} [\d]{1,2}:[\d]{1,2}:[\d]{1,2}$/', $date);
     }
 
@@ -23,70 +57,70 @@ class Validate{
     /**
      *  检查手机号码
      */
-    public static  function check_mobile($mobile = ''){
+    public static  function mobile($mobile = ''){
         return preg_match("/^1[3|4|5|7|8][0-9]\d{8}$/", $mobile);
     }
 
     /**
      *  检查email格式
      */
-    public static function check_email($email = ''){
+    public static function email($email = ''){
         return preg_match("/^[\w\-\.]+@[\w\-]+(\.\w+)+$/", $email);
     }
 
     /**
      *  检查邮政编码
      */
-    public static function check_postal_code($postal_code = ''){
+    public static function postal_code($postal_code = ''){
         return preg_match("/[1-9]{1}(\d+){5}/", $postal_code);
     }
 
     /**
      *  检查ipv4 地址
      */
-    public static function check_ip($ip){
+    public static function ip($ip){
         return preg_match("/(\d+){1,3}\.(\d+){1,3}\.(\d+){1,3}\.(\d+){1,3}/", $ip);
     }
 
     /**
      *  检查qq号码
      */
-    public static  function check_qq($qq = ''){
+    public static  function qq($qq = ''){
         return preg_match("/^[1-9](\d){4,11}$/", $qq);
     }
 
     /**
      *  检查身份证号码
      */
-    public static  function check_id_card($id_card = ''){
+    public static  function id_card($id_card = ''){
         return ( preg_match("/^\d{17}(\d|x)$/i", $id_card) || preg_match("/^\d{15}$/i", $id_card) );
     }
 
     /**
      *  检查性别
      */
-    public static function check_gender($gender){
+    public static function gender($gender){
         return in_array($gender, array(0, 1));
     }
 
     /**
      *  检查产品编号
      */
-    public static function check_product_no($product_no = ''){
+    public static function product_no($product_no = ''){
         return preg_match('/^[0-9a-zA-Z-]{1,16}$/', $product_no);
     }
 
     /**
      *  检查电话号码
      */
-    public static function check_telephone($telephone = ''){
+    public static function telephone($telephone = ''){
         return preg_match( "/^[\d]+[\d-]*[\d]$/", $telephone);
     }
 
     /**
      *  url地址(简单检查是否以http://开头)
      */
-    public static function check_url($url = ''){
+    public static function url($url = ''){
         return preg_match('/^http[s]?:\/\/.*?/i', $url);
     }
 
@@ -106,10 +140,265 @@ class Validate{
     xAC00-xD7A3 (韩文)
     u0800-u4e00 (日文)
      */
-    public static function check_chinese($str){
+    public static function chinese($str){
         //return preg_match('/^[\xa1-\xff]+$/', $str);
         return preg_match('/^[\x80-\xff]+$/', $str);
     }
 
+
+    /**
+     * Required
+     *
+     * @access	public
+     * @param	string
+     * @return	bool
+     */
+    public static function required($str)
+    {
+        if ( ! is_array($str))
+        {
+            return (trim($str) == '') ? FALSE : TRUE;
+        }
+        else
+        {
+            return ( ! empty($str));
+        }
+    }
+
+
+    /**
+     * Minimum Length
+     *
+     * @access	public
+     * @param	string
+     * @param	value
+     * @return	bool
+     */
+    public static function min_length($str, $val)
+    {
+        if (preg_match("/[^0-9]/", $val))
+        {
+            return FALSE;
+        }
+
+        if (function_exists('mb_strlen'))
+        {
+            return (mb_strlen($str) < $val) ? FALSE : TRUE;
+        }
+
+        return (strlen($str) < $val) ? FALSE : TRUE;
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Max Length
+     *
+     * @access	public
+     * @param	string
+     * @param	value
+     * @return	bool
+     */
+    public static function max_length($str, $val)
+    {
+        if (preg_match("/[^0-9]/", $val))
+        {
+            return FALSE;
+        }
+
+        if (function_exists('mb_strlen'))
+        {
+            return (mb_strlen($str) > $val) ? FALSE : TRUE;
+        }
+
+        return (strlen($str) > $val) ? FALSE : TRUE;
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Exact Length
+     *
+     * @access	public
+     * @param	string
+     * @param	value
+     * @return	bool
+     */
+    public static function exact_length($str, $val)
+    {
+        if (preg_match("/[^0-9]/", $val))
+        {
+            return FALSE;
+        }
+
+        if (function_exists('mb_strlen'))
+        {
+            return (mb_strlen($str) != $val) ? FALSE : TRUE;
+        }
+
+        return (strlen($str) != $val) ? FALSE : TRUE;
+    }
+
+    // --------------------------------------------------------------------
+
+
+    /**
+     * Numeric
+     *
+     * @access	public
+     * @param	string
+     * @return	bool
+     */
+    public static function numeric($str)
+    {
+        return (bool)preg_match( '/^[\-+]?[0-9]*\.?[0-9]+$/', $str);
+
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Is Numeric
+     *
+     * @access	public
+     * @param	string
+     * @return	bool
+     */
+    public static function is_numeric($str)
+    {
+        return ( ! is_numeric($str)) ? FALSE : TRUE;
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Integer
+     *
+     * @access	public
+     * @param	string
+     * @return	bool
+     */
+    public static function integer($str)
+    {
+        return (bool) preg_match('/^[\-+]?[0-9]+$/', $str);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Decimal number
+     *
+     * @access	public
+     * @param	string
+     * @return	bool
+     */
+    public static function decimal($str)
+    {
+        return (bool) preg_match('/^[\-+]?[0-9]+\.[0-9]+$/', $str);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Greather than
+     *
+     * @access	public
+     * @param	string
+     * @return	bool
+     */
+    public static  function greater_than($str, $min)
+    {
+        if ( ! is_numeric($str))
+        {
+            return FALSE;
+        }
+        return $str > $min;
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Less than
+     *
+     * @access	public
+     * @param	string
+     * @return	bool
+     */
+    public static function less_than($str, $max)
+    {
+        if ( ! is_numeric($str))
+        {
+            return FALSE;
+        }
+        return $str < $max;
+    }
+
+    /**
+     * Valid Base64
+     *
+     * Tests a string for characters outside of the Base64 alphabet
+     * as defined by RFC 2045 http://www.faqs.org/rfcs/rfc2045
+     *
+     * @access	public
+     * @param	string
+     * @return	bool
+     */
+    public static function base64($str)
+    {
+        return (bool) ! preg_match('/[^a-zA-Z0-9\/\+=]/', $str);
+    }
+
 }
 
+/*
+ *
+ *
+ *
+ 用法：
+<?php
+require './helper/Loader.php';
+class Val{
+
+    public $rules = [
+        'name'=>  array(Validate::DATE,Validate::REQUIRED),
+        'time' => Validate::TIME,
+        'sno'  => Validate::INTEGER,
+
+    ];
+
+    public $data = array(
+        'name'=>'',
+        'time'=>'test',
+        'sno'=>'test',
+    );
+
+    public $_errMessage = array();
+
+
+    public function run()
+    {
+        foreach($this->rules as $key=>$rule){
+               if(is_array($rule)){
+                    foreach($rule as $sub_rule){
+                        if(!Validate::$sub_rule($this->data[$key])){
+                            $this->_errMessage[$key] = Validate::$errorMessage[$sub_rule];
+                        }
+                    }
+               }else{
+                   if(!Validate::$rule($this->data[$key])){
+                       $this->_errMessage[$key] = Validate::$errorMessage[$rule];
+                   }
+               }
+
+        }
+    }
+}
+
+$val = new Val();
+$val->run();
+echo "<pre>";
+ArrayHelper::p($val->_errMessage);
+ *
+ *
+ *
+ */
