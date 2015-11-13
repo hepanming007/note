9624e355e1b32115a93f374a27400640220f09da
hepanming007 <hepanming007@126.com>
2015-11-13 09:13:45 +0800

    Update ArrayToXml.php

--- php/ArrayToXml.php
+++ php/ArrayToXml.php
@@ -21,27 +21,70 @@ class ArrayToXml {
      */
     public static $format_xml = true;
 
-    /**
+     /**
      * 转换数组为 XML
      *
-     * @param  array  $arr               要转换的数组
-     * @param  string $root_element_name 根节点的名称
-     * @return string                    转换后的 XML
+     * 使用示例：
+     * 1. _不_带相同key的传参方式
+     * ```php
+     * $data = array(
+     *      'DepartCity'    => 'WUH',
+     *      'ArrivalCity'   => 'PEK',
+     *      'DepartDate'    => '2015-12-24',
+     * );
+     * Cncn\ArrayToXml::convert($data)
+     * ```
+     * 
+     * 生成 xml
+     * ```xml
+     * <Request>
+     *   <DepartCity>WUH</DepartCity>
+     *   <ArrivalCity>PEK</ArrivalCity>
+     *   <DepartDate>2015-12-24</DepartDate>
+     * </Request>
+     * ```
+     * 
+     * 2. 带相同key的传参方式
+     * ```php
+     * $data = array(
+     *     'passenger' => array(
+     *         0 => array('name' => 'John Doe', 'gender' => 'male'),
+     *         1 => array('name' => 'Jane Doe', 'gender' => 'female'),
+     *     ),
+     * );
+     * Cncn\ArrayToXml::convert($data, 'passengers');
+     * ```
+     * 
+     * 生成 xml
+     * ```xml
+     * <passengers>
+     *   <passenger>
+     *     <name>John Doe</name>
+     *     <gender>male</gender>
+     *   </passenger>
+     *   <passenger>
+     *     <name>Jane Doe</name>
+     *     <gender>female</gender>
+     *   </passenger>
+     * </passengers>
+     * ```
+     *
+     * @param  array  $arr       要转换的数组
+     * @param  string $root_name 根节点的名称
+     * @return string            转换后的 XML
      */
-    public static function convert(array $arr, $root_element_name = 'Request') {
+    public static function convert(array $arr, $root_name = 'Request') {
         $dom = new DOMDocument();
 
         if (self::$format_xml) {
             $dom->preserveWhiteSpace = false;
-            $dom->formatOutput = true;
+            $dom->formatOutput       = true;
         }
 
-        $root = $dom->createElement($root_element_name);
-
+        $root = $dom->createElement($root_name);
         foreach ($arr as $key => $value) {
-            $root->appendChild(self::convert_element($value, $key, $dom));
+            self::convert_node($value, $key, $root);
         }
-
         $dom->appendChild($root);
 
         /* $dom->documentElement 去掉 <?xml version="1.0"?> 声明 */
@@ -51,21 +94,28 @@ class ArrayToXml {
     /**
      * 转换节点元素
      *
-     * @param  string|string[]    
-     * @param  string      $key
-     * @param  \DOMDocument $dom   
-     * @return \DOMElement
+     * @param  string|string[] $node_value 节点元素的值
+     * @param  string          $node_name  节点元素的名称
+     * @param  \DOMNode        $parent     父节点
      */
-    protected static function convert_element($value, $key, DOMDocument $dom) {
-        $element = $dom->createElement($key);
-        if (is_array($element)) {
-            foreach ($element as $key => $value) {
-                $element->appendChild(self::convert_element($value, $key, $dom));
+    protected static function convert_node($node_value, $node_name, $parent) {
+        if (is_array($node_value)) {
+            foreach ($node_value as $key => $value) {
+                $child = $parent->appendChild(new DOMElement($node_name));
+                if (is_array($value)) {
+                    if (!is_numeric($key)) {
+                        self::convert_node($value, $key, $child);
+                    } else {
+                        foreach ($value as $k => $v) {
+                            self::convert_node($v, $k, $child);
+                        }
+                    }
+                } else {
+                    $child->appendChild(new DOMElement($key, $value));
+                }
             }
         } else {
-            $element->nodeValue = $value;
+            $parent->appendChild(new DOMElement($node_name, $node_value));
         }
-
-        return $element;
     }
 }
