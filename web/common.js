587f5044c82c76038d0fd331620840c92a6c792f
hepanming007 <hepanming007@126.com>
2016-08-31 09:21:32 +0800

    Create common.js

---
+++ web/common.js
@@ -0,0 +1,25 @@
+var validate_from={
+    length:function(str){
+        var len = 0;
+        for (var i=0; i<str.length; i++) {
+            if (str.charCodeAt(i)>127 || str.charCodeAt(i)==94) {
+                len += 2;
+            } else {
+                len ++;
+            }
+        }
+        return len;
+    },
+    empty:function(str){
+      return str.length==0;
+    },
+    mobile:function(str){
+        var reg = /^1[3|4|5|7|8][0-9]\d{8}$/;
+        return reg.test(str)
+    },
+    card_number:function(str){
+        var reg1 = /^\d{17}(\d|x)$/;
+        var reg2 = /^\d{15}$/;
+        return reg1.test(str) || reg2.test(str);
+    }
+};
